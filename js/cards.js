1363336c259586a4bd5e3745ddec186d2ad43ef4
Samuel Brandão <74151920+budes@users.noreply.github.com>
2022-09-13 20:50:57 -0300

    Add files via upload

---
+++ js/cards.js
@@ -0,0 +1,40 @@
+let cards = [
+    //["Especiais/agua_parada.png", "x2", 1, "agua"],
+    //["Especiais/chinelo.png", "1p", 1, "chinelo"],
+    //["Especiais/clima.png", "x2", 1, "clima"], //insetos
+    //["Especiais/higiene.png", "/2", 1, "higiene"],
+    //["Especiais/inseticida.png", "1p", 1, "inseticida"],
+    //["Especiais/limpeza.png", "/2", 1, "limpeza"],
+    ["Especiais/Quarto sujo.png", "x2", 1, "quarto"], //insetos/aracnideos
+    ["Especiais/Quarto sujo.png", "x2", 1, "quarto"], //insetos/aracnideos
+    ["Especiais/Quarto sujo.png", "x2", 1, "quarto"], //insetos/aracnideos
+    ["Especiais/Quarto sujo.png", "x2", 1, "quarto"], //insetos/aracnideos
+    ["Especiais/Quarto sujo.png", "x2", 1, "quarto"], //insetos/aracnideos
+    //["Especiais/remedio_verme.png", "1p", 1, "remedio"],
+    //["Especiais/repelente.png", "/2", 1, "repelente"],
+    ["Especiais/Sem saneamento.png", "x2", 1, "saneamento"], //nematodeos
+    ["Especiais/Sem saneamento.png", "x2", 1, "saneamento"], //nematodeos
+    ["Especiais/Sem saneamento.png", "x2", 1, "saneamento"], //nematodeos
+    ["Especiais/Sem saneamento.png", "x2", 1, "saneamento"], //nematodeos
+    ["Especiais/Sem saneamento.png", "x2", 1, "saneamento"], //nematodeos
+
+    ["Ectoparasitas/Aedes aegypti.png", 10, 2, "aedes"],
+    ["Ectoparasitas/Amblyomma cajennense.png", 14, 2, "amblyomma"],
+    ["Ectoparasitas/Anopheles darlingi.png", 18, 2, "anopheles"],
+    ["Ectoparasitas/Lutzomyia longipalpis.png", 12, 2, "lutzomyia"],
+    ["Ectoparasitas/Panstrongylus megistus.png", 20, 2, "panstrongylus"],
+    ["Ectoparasitas/Pediculus humanus.png", 12, 2, "pediculus"],
+    ["Ectoparasitas/Rhodnius prolixus.png", 16, 2, "rhodnius"],
+    ["Ectoparasitas/Sarcoptes scarbiei.png", 14, 2, "sarcoptes"],
+    ["Ectoparasitas/Triatoma infestans.png", 12, 2, "triatoma"],
+    ["Ectoparasitas/Xenopsylla cheopis.png", 16, 2, "xenopsylia"],
+
+    ["Endoparasitas/Ancylostoma brasiliensis.png", 20, 3, "brasiliensis"],
+    ["Endoparasitas/Ancylostoma caninum.png", 10, 3, "caninum"],
+    ["Endoparasitas/Ascaris lumbricoides.png", 14, 3, "ascaris"],
+    ["Endoparasitas/Necator americanus.png", 18, 3, "necator"],
+    ["Endoparasitas/Oxyurus vermicularis.png", 12, 3, "oxyurus"],
+    ["Endoparasitas/Strongyloides stercoralis.png", 18, 3, "strongyloides"],
+    ["Endoparasitas/Trichuris trichiura.png", 14, 3, "trichuris"],
+    ["Endoparasitas/Wucheria bancrofti.png", 20, 3, "wucheria"]
+]
--- js/main.js
+++ js/main.js
@@ -1,18 +1,24 @@
 let en_layer1 = []
 let en_layer2 = []
+let en_layer3 = []
 let en_deck = []
 
 let pl_layer1 = []
 let pl_layer2 = []
-let pl_deck = [["ascaris_lumbricoides.png", 12, 1, "ascaris"], 
-                ["ascaris_lumbricoides.png", 12, 1, "ascaris"], 
-                ["amblyomma_cajennense.png", 14, 2, "amblyomma"],
-                ["amblyomma_cajennense.png", 14, 2, "amblyomma"],
-                ["aedes_aegypti.png", 10, 3, "aedes"], 
-                ["aedes_aegypti.png", 10, 3, "aedes"],
-                ["aedes_aegypti.png", 10, 3, "aedes"]]
+let pl_layer3 = []
+let pl_deck = []
 
-function deckExists() {
+/* Player's deck creation */
+function getRandomCards(number_of_cards) {
+    /* Selects random cards to put on the pl_deck array */
+    for (var repetitions = 1; repetitions <= number_of_cards; repetitions++) {
+        pl_deck.push(cards[Math.floor(Math.random() * cards.length)])
+    }
+}
+getRandomCards(10)
+
+function deckCreate() {
+    /* Creates the deck dinamically based on the values on pl_deck */
     var area = document.getElementById("cards-selection")
     var html = ""
     
@@ -20,8 +26,8 @@ function deckExists() {
 
         /* It's on the same line, i just thought that it would be easier to read this way */
         html += '<div id="' + pl_deck[i][3] + '" '
-        html += 'onclick="Click([' + "'" + pl_deck[i][0].trim() + "', " + pl_deck[i][1] + ", " + pl_deck[i][2] + ", '" + pl_deck[i][3].trim() + "'" + '])" '
-        html +='style="background-image: url(' + "'../images/" + pl_deck[i][0] + "')" + '" ' + 'class="card">'
+        html += 'onclick="Click([' + "'" + pl_deck[i][0].trim() + "', '" + pl_deck[i][1] + "', " + pl_deck[i][2] + ", '" + pl_deck[i][3].trim() + "'" + '])" '
+        html +='style="background-image: url(' + "'../images/cards/" + pl_deck[i][0] + "')" + '" ' + 'class="card">'
         
         html += "\t" + '<div class="level">' + pl_deck[i][1] + "</div>"
         html += "</div>"
@@ -29,21 +35,40 @@ function deckExists() {
 
     area.innerHTML = html
 }
-deckExists()
+deckCreate()
 
+/* Funcionality of the cards */
 function Click(info) {
+    /* Makes the card move from the deck to the game */
     var old_area = document.querySelectorAll("#cards-selection #" + info[3])
 
     var new_html = document.querySelectorAll("#player #l" + info[2])[0].innerHTML
-    new_html += '<div id="' + info[2] + '" onclick="Click(' + info[2] + ')" style="background-image: url(' + "'images/"+ info[0] + "')" + '" ' + 'class="card">' + "\n"
+    new_html += '<div id="' + info[2] + '" onclick="Click(' + info[2] + ')" style="background-image: url(' + "'images/cards/"+ info[0] + "')" + '" ' + 'class="card">' + "\n"
     new_html += "\t" + '<div class="level">' + info[1] + "</div>" + "\n"
     new_html += "</div>" + "\n"
 
     document.querySelectorAll("#player #l" + info[2])[0].innerHTML = new_html
 
     old_area[0].remove()
+    
+    if (info[2] == 1) {
+        pl_layer1.push(info)
+        
+        if (info[3] == "quarto") {
+            changeCardsPoints(2, 0)
+        }
+        if (info[3] == "saneamento") {
+            changeCardsPoints(3, 0)
+        }
+        
+    } else if(info[2] == 2) {
+        pl_layer2.push(info)
+    } else {
+        pl_layer3.push(info)
+    }
 }
 
+/* The creation of the board, with the right proportion of the image and hitbox */
 function boardCreation(source, height) {
     // Every value atributed to the height will be converted to the rem units
 
@@ -62,8 +87,30 @@ function boardCreation(source, height) {
     game_board.style.width = img_aspect_ratio * height + "rem"
 
 }
+boardCreation("/images/Tabuleiro_3.png", 34)
 
-boardCreation("/images/Tabuleiro_1.png", 43)
+/* The score */
+function changeCardsPoints(layer, action) {
+    var elements = document.querySelectorAll("#player #l" + layer + " div .level")
+
+    if (action == 0) { // Duplication (2x)
+        for (var i = 0; i < elements.length; i++) {
+            var value = elements[i].innerHTML
+            elements[i].innerHTML = parseInt(value) * 2
+        }
+    } else if (action == 1) { // Split by half (/2)
+        for (var i = 0; i < elements.length; i++) {
+            var value = elements[i].innerHTML
+            elements[i].innerHTML = parseInt(parseInt(value) / 2)
+        }
+    } else { // Reset to 1
+        for (var i = 0; i < elements.length; i++) {
+            var value = elements[i].innerHTML
+            elements[i].innerHTML = 1
+        }
+    }
+
+}   
 
 /*
 const card = document.getElementsByClassName("card");
