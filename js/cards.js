8ca1b04fa8ab5929dcc378f20599badd027ec904
Samuel Brandão <74151920+budes@users.noreply.github.com>
2022-09-13 21:16:28 -0300

    Update cards.js

--- js/cards.js
+++ js/cards.js
@@ -1,40 +1,40 @@
 let cards = [
-    //["Especiais/agua_parada.png", "x2", 1, "agua"],
-    //["Especiais/chinelo.png", "1p", 1, "chinelo"],
-    //["Especiais/clima.png", "x2", 1, "clima"], //insetos
-    //["Especiais/higiene.png", "/2", 1, "higiene"],
-    //["Especiais/inseticida.png", "1p", 1, "inseticida"],
-    //["Especiais/limpeza.png", "/2", 1, "limpeza"],
-    ["Especiais/Quarto sujo.png", "x2", 1, "quarto"], //insetos/aracnideos
-    ["Especiais/Quarto sujo.png", "x2", 1, "quarto"], //insetos/aracnideos
-    ["Especiais/Quarto sujo.png", "x2", 1, "quarto"], //insetos/aracnideos
-    ["Especiais/Quarto sujo.png", "x2", 1, "quarto"], //insetos/aracnideos
-    ["Especiais/Quarto sujo.png", "x2", 1, "quarto"], //insetos/aracnideos
-    //["Especiais/remedio_verme.png", "1p", 1, "remedio"],
-    //["Especiais/repelente.png", "/2", 1, "repelente"],
-    ["Especiais/Sem saneamento.png", "x2", 1, "saneamento"], //nematodeos
-    ["Especiais/Sem saneamento.png", "x2", 1, "saneamento"], //nematodeos
-    ["Especiais/Sem saneamento.png", "x2", 1, "saneamento"], //nematodeos
-    ["Especiais/Sem saneamento.png", "x2", 1, "saneamento"], //nematodeos
-    ["Especiais/Sem saneamento.png", "x2", 1, "saneamento"], //nematodeos
+    //["especiais/agua_parada.png", "x2", 1, "agua"],
+    //["especiais/chinelo.png", "1p", 1, "chinelo"],
+    //["especiais/clima.png", "x2", 1, "clima"], //insetos
+    //["especiais/higiene.png", "/2", 1, "higiene"],
+    //["especiais/inseticida.png", "1p", 1, "inseticida"],
+    //["especiais/limpeza.png", "/2", 1, "limpeza"],
+    ["especiais/quarto_sujo.png", "x2", 1, " quarto"], //insetos/aracnideos
+    ["especiais/quarto_sujo.png", "x2", 1, " quarto"], //insetos/aracnideos
+    ["especiais/quarto_sujo.png", "x2", 1, " quarto"], //insetos/aracnideos
+    ["especiais/quarto_sujo.png", "x2", 1, " quarto"], //insetos/aracnideos
+    ["especiais/quarto_sujo.png", "x2", 1, "quarto"], //insetos/aracnideos
+    //["especiais/remedio_verme.png", "1p", 1, "remedio"],
+    //["especiais/repelente.png", "/2", 1, "repelente"],
+    ["especiais/sem_saneamento.png", "x2", 1, "saneamento"], //nematodeos
+    ["especiais/sem_saneamento.png", "x2", 1, "saneamento"], //nematodeos
+    ["especiais/Sem_saneamento.png", "x2", 1, "saneamento"], //nematodeos
+    ["especiais/sem_saneamento.png", "x2", 1, "saneamento"], //nematodeos
+    ["especiais/sem_saneamento.png", "x2", 1, "saneamento"], //nematodeos
 
-    ["Ectoparasitas/Aedes aegypti.png", 10, 2, "aedes"],
-    ["Ectoparasitas/Amblyomma cajennense.png", 14, 2, "amblyomma"],
-    ["Ectoparasitas/Anopheles darlingi.png", 18, 2, "anopheles"],
-    ["Ectoparasitas/Lutzomyia longipalpis.png", 12, 2, "lutzomyia"],
-    ["Ectoparasitas/Panstrongylus megistus.png", 20, 2, "panstrongylus"],
-    ["Ectoparasitas/Pediculus humanus.png", 12, 2, "pediculus"],
-    ["Ectoparasitas/Rhodnius prolixus.png", 16, 2, "rhodnius"],
-    ["Ectoparasitas/Sarcoptes scarbiei.png", 14, 2, "sarcoptes"],
-    ["Ectoparasitas/Triatoma infestans.png", 12, 2, "triatoma"],
-    ["Ectoparasitas/Xenopsylla cheopis.png", 16, 2, "xenopsylia"],
+    ["ectoparasitas/aedes_aegypti.png", 10, 2, "aedes"],
+    ["ectoparasitas/amblyomma_cajennense.png", 14, 2, "amblyomma"],
+    ["ectoparasitas/anopheles_darlingi.png", 18, 2, "anopheles"],
+    ["ectoparasitas/lutzomyia_longipalpis.png", 12, 2, "lutzomyia"],
+    ["ectoparasitas/panstrongylus_megistus.png", 20, 2, "panstrongylus"],
+    ["ectoparasitas/pediculus_humanus.png", 12, 2, "pediculus"],
+    ["ectoparasitas/rhodnius_prolixus.png", 16, 2, "rhodnius"],
+    ["ectoparasitas/sarcoptes_scarbiei.png", 14, 2, "sarcoptes"],
+    ["ectoparasitas/triatoma_infestans.png", 12, 2, "triatoma"],
+    ["ectoparasitas/xenopsylla cheopis.png", 16, 2, "xenopsylia"],
 
-    ["Endoparasitas/Ancylostoma brasiliensis.png", 20, 3, "brasiliensis"],
-    ["Endoparasitas/Ancylostoma caninum.png", 10, 3, "caninum"],
-    ["Endoparasitas/Ascaris lumbricoides.png", 14, 3, "ascaris"],
-    ["Endoparasitas/Necator americanus.png", 18, 3, "necator"],
-    ["Endoparasitas/Oxyurus vermicularis.png", 12, 3, "oxyurus"],
-    ["Endoparasitas/Strongyloides stercoralis.png", 18, 3, "strongyloides"],
-    ["Endoparasitas/Trichuris trichiura.png", 14, 3, "trichuris"],
-    ["Endoparasitas/Wucheria bancrofti.png", 20, 3, "wucheria"]
+    ["endoparasitas/ancylostoma_brasiliensis.png", 20, 3, "brasiliensis"],
+    ["endoparasitas/ancylostoma_caninum.png", 10, 3, "caninum"],
+    ["endoparasitas/ascaris_lumbricoides.png", 14, 3, "ascaris"],
+    ["endoparasitas/necator_americanus.png", 18, 3, "necator"],
+    ["endoparasitas/oxyurus_vermicularis.png", 12, 3, "oxyurus"],
+    ["endoparasitas/strongyloides_stercoralis.png", 18, 3, "strongyloides"],
+    ["endoparasitas/trichuris_trichiura.png", 14, 3, "trichuris"],
+    ["endoparasitas/wucheria_bancrofti.png", 20, 3, "wucheria"]
 ]
